730b443195a91f58ce58a316a9ba96628b4b5d30
James Spencer <james.spencer@news.co.uk>
2019-10-28 19:46:09 +0000

    basic graph ql server setup

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import graphqlHttp from 'express-graphql';
+import schema from './schema';
 
 const app = express(); 
 
 app.get('/', (req, res) => {
     res.send('GraphQL is amazing!');
 });
+
+
+const root = { hello : () => "Hi, I'm James" };
+
+app.use('/graphql', graphqlHttp({
+    schema,
+    rootValue: root,
+    graphiql : true, 
+}));
 
 app.listen(8080, () => console.log('Running server on port localhost:8080/graphql'));
